0f96ecf73fdb0569a5177967d463c45eb6903a8d
Ted M. Young <tedyoung@gmail.com>
2023-02-22 15:27:44 -0800

    LAB #4: Tidied up Suit to remove scaffolding

--- src/main/java/com/jitterted/ebp/blackjack/Suit.java
+++ src/main/java/com/jitterted/ebp/blackjack/Suit.java
@@ -1,7 +1,5 @@
 package com.jitterted.ebp.blackjack;
 
-import java.util.List;
-
 public enum Suit {
     HEARTS("♥"),
     CLUBS("♣"),
@@ -9,23 +7,10 @@ public enum Suit {
     SPADES("♠")
     ;
 
-    @Deprecated // duplicate info
-    static final List<String> SUITS = List.of("♠", "♦", "♥", "♣");
-
     private final String symbol;
 
     Suit(String symbol) {
         this.symbol = symbol;
-    }
-
-    @Deprecated // "Scaffolding"
-    public static Suit from(String symbol) {
-        for (Suit suit : Suit.values()) {
-            if (symbol.equals(suit.symbol())) {
-                return suit;
-            }
-        }
-        return null;
     }
 
     public String symbol() {
